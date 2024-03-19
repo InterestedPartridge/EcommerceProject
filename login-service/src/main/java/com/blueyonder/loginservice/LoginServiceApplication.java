86d89de8abcd7eccf6a995aa0200839dc01cd71d
InterestedPartridge <bichpuriar471@gmai.com>
2024-03-19 14:18:15 +0530

    initial commit

---
+++ login-service/src/main/java/com/blueyonder/loginservice/LoginServiceApplication.java
@@ -0,0 +1,20 @@
+package com.blueyonder.loginservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@OpenAPIDefinition(info = @Info(title = "Customer api", description = "Documentation customer API v1.0"))
+//@OpenAPIDefinition(info = @Info(title = "ShopService api", description = "Documentation shopService API v1.0"))
+
+public class LoginServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginServiceApplication.class, args);
+	}
+
+}
